8a29e22cb7b69f9d2c00ecf2c7566439471d9ff0
Parag Chimankar <141899674+Chimankarparag@users.noreply.github.com>
2025-05-21 20:59:21 +0530

    npc A is working just fine , npc B isnt responding

--- Assets/Scripts/NPCInteraction.cs
+++ Assets/Scripts/NPCInteraction.cs
@@ -1,14 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class NPCInteraction : MonoBehaviour
 {
-    public ChatUIManager chatManager;
+    [Header("NPC Settings")]
+    public string npcPrompt;
 
+    [Header("References")]
+    public GameObject chatCanvas;
+    public ServerRunner serverRunner;
+    public GameObject player;
+    public Button leaveButton;
+    private bool isPlayerNear = false;
+    private bool isChatting = false;
+    private bool serverStarted = false;
+    void Start()
+    {
+        chatCanvas.SetActive(false);
+        leaveButton.gameObject.SetActive(false);
+        leaveButton.onClick.AddListener(LeaveChat);
+
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            chatManager.EnableConversation();
+            isPlayerNear = true;
+
+            if (!ServerRunner.IsRunning)
+                serverRunner.LaunchServerManually();
+
+            if (!isChatting)
+            {
+                // serverRunner.SendPromptToServer(npcPrompt);
+                EnableConversation();
+            }
         }
     }
 
@@ -16,7 +43,40 @@ public class NPCInteraction : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            chatManager.DisableConversation();
+            isPlayerNear = false;
+            if (!isChatting) chatCanvas.SetActive(false);
         }
     }
+    public void EnableConversation()
+    {
+
+        isChatting = true;
+        chatCanvas.SetActive(true);
+        leaveButton.gameObject.SetActive(true);
+        SwitchToUIMap();
+    }
+
+    private void LeaveChat()
+    {
+        isChatting = false;
+        chatCanvas.SetActive(false);
+        leaveButton.gameObject.SetActive(false);
+        SwitchToPlayerMap();
+    }
+
+
+    private void SwitchToUIMap()
+    {
+        var input = player.GetComponent<PlayerInput>();
+        input?.SwitchCurrentActionMap("UI");
+        Debug.Log("Switched to UI map");
+    }
+
+    private void SwitchToPlayerMap()
+    {
+        var input = player.GetComponent<PlayerInput>();
+        input?.SwitchCurrentActionMap("Player");
+         Debug.Log("Switched to Player map");
+    }
+
 }
